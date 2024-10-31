3ea34aa9537e1aae793e2c9f5835dcb07f172c11
ComActivity <104488499+ComActivity@users.noreply.github.com>
2024-10-31 21:54:56 +1100

    EXT850MI_AddAttributes.groovy

--- EXT850MI_AddAttributes.groovy
+++ EXT850MI_AddAttributes.groovy
@@ -21,25 +21,23 @@
  ***************************************************************
  */
  
- import groovy.lang.Closure
- 
+ import groovy.lang.Closure;
  import java.time.LocalDate;
  import java.time.LocalDateTime;
  import java.time.format.DateTimeFormatter;
  import java.time.ZoneId;
  
- 
  /*
  *Modification area - M3
  *Nbr               Date      User id     Description
- *BF_R_2001         20241015  DRIESR      Update Attributes in batch mode
+ *BF_R_2001         20241015  DRIESR      Add/Update Attributes in ATS101 for multiple lot numbers
  *
  */
  
  /*
-  * AP paymnent split night run
+  * Add/Update Attributes for Grading 2.0 H5 SDK
  */
-public class UpdAttributes extends ExtendM3Transaction {
+public class AddAttributes extends ExtendM3Transaction {
   private final MIAPI mi;
   private final DatabaseAPI database;
   private final MICallerAPI miCaller;
@@ -56,16 +54,16 @@ public class UpdAttributes extends ExtendM3Transaction {
   private String itno;
   private String atnr;
   private String atva;
+  private String txt1;
   private int XXCONO;
   
-  public UpdAttributes(MIAPI mi, DatabaseAPI database, MICallerAPI miCaller, LoggerAPI logger, ProgramAPI program,  IonAPI ion) {
+  public AddAttributes(MIAPI mi, DatabaseAPI database, MICallerAPI miCaller, LoggerAPI logger, ProgramAPI program,  IonAPI ion) {
     this.mi = mi;
     this.database = database;
     this.miCaller = miCaller;
     this.logger = logger;
     this.program = program;
     this.ion = ion;
-
   }
   
   public void main() {
@@ -73,58 +71,113 @@ public class UpdAttributes extends ExtendM3Transaction {
   	if (divi == "?") {
   	  divi = "";
   	} 
-  	
   	faci = mi.inData.get("FACI") == null ? '' : mi.inData.get("FACI").trim();
   	if (faci == "?") {
   	  faci = "";
   	}
-  	
-  	itno = mi.inData.get("ITNO") == null ? '' : mi.inData.get("ITNO").trim();
-  	if (itno == "?") {
-  	  itno = "";
-  	}
-  	
   	bref = mi.inData.get("BREF") == null ? '' : mi.inData.get("BREF").trim();
   	if (bref == "?") {
   	  bref = "";
   	}
-  	
+  	itno = mi.inData.get("ITNO") == null ? '' : mi.inData.get("ITNO").trim();
+  	if (itno == "?") {
+  	  itno = "";
+  	}
   	atid = mi.inData.get("ATID") == null ? '' : mi.inData.get("ATID").trim();
   	if (atid == "?") {
   	  atid = "";
   	}
-  	
-  
   	atva = mi.inData.get("ATVA") == null ? '' : mi.inData.get("ATVA").trim();
   	if (atva == "?") {
   	  atva = "";
   	}
+  	txt1 = mi.inData.get("TXT1") == null ? '' : mi.inData.get("TXT1").trim();
+  	if (txt1 == "?") {
+  	  txt1 = "";
+  	}
   	
-  
   	if (divi.isEmpty()) {
-      mi.error("Division must be entered");
+      mi.error("division must be entered");
       return;
     }
-    //A WZHAO 20230328 - new input parameter XJTM
-  	xjtm = mi.inData.get("XJTM") == null ? '' : mi.inData.get("XJTM").trim();
-  	if (xjtm == "?") {
-  	  xjtm = "";
-  	}
+    if (faci.isEmpty()) {
+      mi.error("facility must be entered");
+      return;
+    }
+    if (bref.isEmpty()) {
+      mi.error("lot reference must be entered");
+      return;
+    }
+    
+    if (atid.equals("CHM04") && txt1.isEmpty()) {
+       mi.error("comments must be entered");
+    }
+    
+    if (!atid.equals("CHM04") && !atid.isEmpty() && atva.isEmpty()) {
+       mi.error("attribute value ust be entered");
+    }
+    
+    
     XXCONO= program.LDAZD.CONO;
     
-    DBAction queryCMNDIV = database.table("CMNDIV").index("00").selection("CCDIVI").build();
-    DBContainer CMNDIV = queryCMNDIV.getContainer();
-    CMNDIV.set("CCCONO", XXCONO);
-    CMNDIV.set("CCDIVI", divi);
-    if(!queryCMNDIV.read(CMNDIV)) {
-      mi.error("Division does not exist.");
-      return;
+     // - validate item number
+    if (!itno.isEmpty()) {
+      DBAction queryMITMAS = database.table("MITMAS").index("00").selection("MMITNO").build();
+      DBContainer MITMAS = queryMITMAS.getContainer();
+      MITMAS.set("MMCONO", XXCONO);
+      MITMAS.set("MMITNO", itno.trim());
+      if (!queryMITMAS.read(MITMAS)) {
+        mi.error("item number does not exist.");
+        return;
+      }
+    }
+
+    // - validate facility
+    if (!faci.isEmpty()) {
+      DBAction queryCFACIL = database.table("CFACIL").index("00").build();
+      DBContainer CFACIL = queryCFACIL.getContainer();
+      CFACIL.set("CFCONO", XXCONO);
+      CFACIL.set("CFFACI", faci);
+      if(!queryCFACIL.read(CFACIL)) {
+        mi.error("facility does not exist.");
+        return;
+      } 
+    }
+ 
+    // - validate attribute ID   
+    if (!atid.isEmpty()) {
+      DBAction queryMATRMA = database.table("MATRMA").index("00").build();
+      DBContainer MATRMA = queryMATRMA.getContainer();
+      MATRMA.set("AACONO", XXCONO);
+      MATRMA.set("AAATID", atid);
+      if(!queryMATRMA.read(MATRMA)) {
+        mi.error("attribute ID does not exist.");
+        return;
+      } 
+    }
+
+    // - validate division
+    if (!divi.isEmpty()) {
+      DBAction queryCMNDIV = database.table("CMNDIV").index("00").build();
+      DBContainer CMNDIV = queryCMNDIV.getContainer();
+      CMNDIV.set("CCCONO", XXCONO);
+      CMNDIV.set("CCDIVI", divi);
+      if(!queryCMNDIV.read(CMNDIV)) {
+        mi.error("Division does not exist.");
+        return;
     } 
-  	String referenceId = UUID.randomUUID().toString();
-    setupData(referenceId);
-      def params = ["JOB": "EXT850", "TX30": "UpdateAttributes", "XCAT": "010", "SCTY": "1", "XNOW": "1", "UUID": referenceId]; // ingle run - now
+  }
+    
+  String referenceId = UUID.randomUUID().toString();
+  setupData(referenceId);
+    
+  if (atid.equals("CHM04")) {
+    def params = ["JOB": "EXT851", "TX30": "UpdateComments", "XCAT": "010", "SCTY": "1", "XNOW": "1", "UUID": referenceId]; // ingle run - now
+    miCaller.call("SHS010MI", "SchedXM3Job", params, { result -> });
+    } else {
+      def params = ["JOB": "EXT850", "TX30": "AddAttributes", "XCAT": "010", "SCTY": "1", "XNOW": "1", "UUID": referenceId]; // ingle run - now
       miCaller.call("SHS010MI", "SchedXM3Job", params, { result -> });
-     
+    }
   }
   /*
 	 * setupData  - write to EXTJOB
@@ -133,7 +186,7 @@ public class UpdAttributes extends ExtendM3Transaction {
   private void setupData(String referenceId) {
     String data = "";
     if (!divi.isEmpty()) {
-      data = divi + ',' + faci + ',' + itno + ',' + bref + ',' + atid + ',' + atva;
+      data = divi + ',' + faci + ',' + itno + ',' + bref + ',' + atid + ',' + atva + ',' + txt1;
     }
     int currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")).toInteger();
   	int currentTime = Integer.valueOf(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HHmmss")));
@@ -151,3 +204,4 @@ public class UpdAttributes extends ExtendM3Transaction {
     actionEXTJOB.insert(EXTJOB);
   }
 }
+
