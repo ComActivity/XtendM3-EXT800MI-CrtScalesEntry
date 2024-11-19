14bca1b8e8213c2263cdd98853c04f482f44f26e
drirem0 <34474688+drirem0@users.noreply.github.com>
2024-11-19 15:37:25 +1100

    Add files via upload
    revert feedback after code changes

---
+++ EXT850MI-AddAttributes.groovy
@@ -0,0 +1,207 @@
+/*
+ ***************************************************************
+ *                                                             *
+ *                           NOTICE                            *
+ *                                                             *
+ *   THIS SOFTWARE IS THE PROPERTY OF AND CONTAINS             *
+ *   CONFIDENTIAL INFORMATION OF INFOR AND/OR ITS AFFILIATES   *
+ *   OR SUBSIDIARIES AND SHALL NOT BE DISCLOSED WITHOUT PRIOR  *
+ *   WRITTEN PERMISSION. LICENSED CUSTOMERS MAY COPY AND       *
+ *   ADAPT THIS SOFTWARE FOR THEIR OWN USE IN ACCORDANCE WITH  *
+ *   THE TERMS OF THEIR SOFTWARE LICENSE AGREEMENT.            *
+ *   ALL OTHER RIGHTS RESERVED.                                *
+ *                                                             *
+ *   (c) COPYRIGHT 2020 INFOR.  ALL RIGHTS RESERVED.           *
+ *   THE WORD AND DESIGN MARKS SET FORTH HEREIN ARE            *
+ *   TRADEMARKS AND/OR REGISTERED TRADEMARKS OF INFOR          *
+ *   AND/OR ITS AFFILIATES AND SUBSIDIARIES. ALL RIGHTS        *
+ *   RESERVED.  ALL OTHER TRADEMARKS LISTED HEREIN ARE         *
+ *   THE PROPERTY OF THEIR RESPECTIVE OWNERS.                  *
+ *                                                             *
+ ***************************************************************
+ */
+ 
+ import groovy.lang.Closure;
+ import java.time.LocalDate;
+ import java.time.LocalDateTime;
+ import java.time.ZoneId;
+ import java.time.format.DateTimeFormatter;
+
+ /*
+ *Modification area - M3
+ *Nbr               Date      User id     Description
+ *BF_R_2001         20241015  DRIESR      Add/Update Attributes in ATS101 for multiple lot numbers
+ *
+ */
+ 
+ /*
+  * Add/Update Attributes for Grading 2.0 H5 SDK
+ */
+public class AddAttributes extends ExtendM3Transaction {
+  private final MIAPI mi;
+  private final DatabaseAPI database;
+  private final MICallerAPI miCaller;
+  private final LoggerAPI logger;
+  private final ProgramAPI program;
+  private final IonAPI ion;
+
+  private String divi;
+  private String faci;
+  private String xnow;
+  private String bref;
+  private String atid;
+  private String xjtm;
+  private String itno;
+  private String atva;
+  private String txt1;
+  private int XXCONO;
+  
+  public AddAttributes(MIAPI mi, DatabaseAPI database, MICallerAPI miCaller, LoggerAPI logger, ProgramAPI program,  IonAPI ion) {
+    this.mi = mi;
+    this.database = database;
+    this.miCaller = miCaller;
+    this.logger = logger;
+    this.program = program;
+    this.ion = ion;
+  }
+  
+  public void main() {
+    divi = mi.inData.get("DIVI") == null ? '' : mi.inData.get("DIVI").trim();
+  	if (divi == "?") {
+  	  divi = "";
+  	} 
+  	faci = mi.inData.get("FACI") == null ? '' : mi.inData.get("FACI").trim();
+  	if (faci == "?") {
+  	  faci = "";
+  	}
+  	bref = mi.inData.get("BREF") == null ? '' : mi.inData.get("BREF").trim();
+  	if (bref == "?") {
+  	  bref = "";
+  	}
+  	itno = mi.inData.get("ITNO") == null ? '' : mi.inData.get("ITNO").trim();
+  	if (itno == "?") {
+  	  itno = "";
+  	}
+  	atid = mi.inData.get("ATID") == null ? '' : mi.inData.get("ATID").trim();
+  	if (atid == "?") {
+  	  atid = "";
+  	}
+  	atva = mi.inData.get("ATVA") == null ? '' : mi.inData.get("ATVA").trim();
+  	if (atva == "?") {
+  	  atva = "";
+  	}
+  	txt1 = mi.inData.get("TXT1") == null ? '' : mi.inData.get("TXT1").trim();
+  	if (txt1 == "?") {
+  	  txt1 = "";
+  	}
+  	
+  	if (divi.isEmpty()) {
+      mi.error("division must be entered");
+      return;
+    }
+    if (faci.isEmpty()) {
+      mi.error("facility must be entered");
+      return;
+    }
+    if (bref.isEmpty()) {
+      mi.error("lot reference must be entered");
+      return;
+    }
+    
+    if (atid.equals("CHM04") && txt1.isEmpty()) {
+      mi.error("comments must be entered");
+      return    
+    }
+    
+    if (!atid.equals("CHM04") && !atid.isEmpty() && atva.isEmpty()) {
+       mi.error("attribute value must be entered");
+      return;
+    }
+    
+    XXCONO= program.LDAZD.CONO;
+    
+     // - validate item number
+    if (!itno.isEmpty()) {
+      DBAction queryMITMAS = database.table("MITMAS").index("00").selection("MMITNO").build();
+      DBContainer MITMAS = queryMITMAS.getContainer();
+      MITMAS.set("MMCONO", XXCONO);
+      MITMAS.set("MMITNO", itno.trim());
+      if (!queryMITMAS.read(MITMAS)) {
+        mi.error("item number does not exist.");
+        return;
+      }
+    }
+
+    // - validate facility
+    if (!faci.isEmpty()) {
+      DBAction queryCFACIL = database.table("CFACIL").index("00").build();
+      DBContainer CFACIL = queryCFACIL.getContainer();
+      CFACIL.set("CFCONO", XXCONO);
+      CFACIL.set("CFFACI", faci);
+      if(!queryCFACIL.read(CFACIL)) {
+        mi.error("facility does not exist.");
+        return;
+      } 
+    }
+ 
+    // - validate attribute ID   
+    if (!atid.isEmpty()) {
+      DBAction queryMATRMA = database.table("MATRMA").index("00").build();
+      DBContainer MATRMA = queryMATRMA.getContainer();
+      MATRMA.set("AACONO", XXCONO);
+      MATRMA.set("AAATID", atid);
+      if(!queryMATRMA.read(MATRMA)) {
+        mi.error("attribute ID does not exist.");
+        return;
+      } 
+    }
+
+    // - validate division
+    if (!divi.isEmpty()) {
+      DBAction queryCMNDIV = database.table("CMNDIV").index("00").build();
+      DBContainer CMNDIV = queryCMNDIV.getContainer();
+      CMNDIV.set("CCCONO", XXCONO);
+      CMNDIV.set("CCDIVI", divi);
+      if(!queryCMNDIV.read(CMNDIV)) {
+        mi.error("Division does not exist.");
+        return;
+    } 
+  }
+    
+  String referenceId = UUID.randomUUID().toString();
+  setupData(referenceId);
+    
+  if (atid.equals("CHM04")) {
+    def params = ["JOB": "EXT851", "TX30": "UpdateComments", "XCAT": "010", "SCTY": "1", "XNOW": "1", "UUID": referenceId]; // ingle run - now
+    miCaller.call("SHS010MI", "SchedXM3Job", params, { result -> });
+    } else {
+      def params = ["JOB": "EXT850", "TX30": "AddAttributes", "XCAT": "010", "SCTY": "1", "XNOW": "1", "UUID": referenceId]; // ingle run - now
+      miCaller.call("SHS010MI", "SchedXM3Job", params, { result -> });
+    }
+  }
+  /*
+	 * setupData  - write to EXTJOB
+	 *
+	 */
+  private void setupData(String referenceId) {
+    String data = "";
+    if (!divi.isEmpty()) {
+      data = divi + ',' + faci + ',' + itno + ',' + bref + ',' + atid + ',' + atva + ',' + txt1;
+    }
+    int currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")).toInteger();
+  	int currentTime = Integer.valueOf(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HHmmss")));
+  	
+    DBAction actionEXTJOB = database.table("EXTJOB").build();
+  	DBContainer EXTJOB = actionEXTJOB.getContainer();
+  	EXTJOB.set("EXCONO", XXCONO);
+  	EXTJOB.set("EXRFID", referenceId);
+  	EXTJOB.set("EXDATA", data);
+    EXTJOB.set("EXRGDT", currentDate);
+  	EXTJOB.set("EXRGTM", currentTime);
+  	EXTJOB.set("EXLMDT", currentDate);
+  	EXTJOB.set("EXCHNO", 0);
+  	EXTJOB.set("EXCHID", program.getUser());
+    actionEXTJOB.insert(EXTJOB);
+  }
+}
+
---
+++ EXT851-Batch.groovy
@@ -0,0 +1,231 @@
+/*
+ ***************************************************************
+ *                                                             *
+ *                           NOTICE                            *
+ *                                                             *
+ *   THIS SOFTWARE IS THE PROPERTY OF AND CONTAINS             *
+ *   CONFIDENTIAL INFORMATION OF INFOR AND/OR ITS AFFILIATES   *
+ *   OR SUBSIDIARIES AND SHALL NOT BE DISCLOSED WITHOUT PRIOR  *
+ *   WRITTEN PERMISSION. LICENSED CUSTOMERS MAY COPY AND       *
+ *   ADAPT THIS SOFTWARE FOR THEIR OWN USE IN ACCORDANCE WITH  *
+ *   THE TERMS OF THEIR SOFTWARE LICENSE AGREEMENT.            *
+ *   ALL OTHER RIGHTS RESERVED.                                *
+ *                                                             *
+ *   (c) COPYRIGHT 2020 INFOR.  ALL RIGHTS RESERVED.           *
+ *   THE WORD AND DESIGN MARKS SET FORTH HEREIN ARE            *
+ *   TRADEMARKS AND/OR REGISTERED TRADEMARKS OF INFOR          *
+ *   AND/OR ITS AFFILIATES AND SUBSIDIARIES. ALL RIGHTS        *
+ *   RESERVED.  ALL OTHER TRADEMARKS LISTED HEREIN ARE         *
+ *   THE PROPERTY OF THEIR RESPECTIVE OWNERS.                  *
+ *                                                             *
+ ***************************************************************
+ */
+ 
+ import groovy.lang.Closure;
+ import java.time.LocalDate;
+ import java.time.LocalDateTime;
+ import java.time.format.DateTimeFormatter;
+ import java.text.DecimalFormat;
+ import java.time.ZoneId;
+ 
+ /*
+ *Modification area - M3
+ *Nbr               Date      User id     Description
+ *BF_R_2001         20241015  DRIESR      Add/Update text attribute  in ATS101 for multiple lot numbers
+ */
+ 
+public class EXT851 extends ExtendM3Batch {
+  private final LoggerAPI logger;
+  private final DatabaseAPI database;
+  private final BatchAPI batch;
+  private final MICallerAPI miCaller;
+  private final ProgramAPI program;
+  
+  private String divi;
+  private String inPYNO;
+  private String inATID;
+  private String inBREF;
+  private String inITNO;
+  private String inATVA;
+  private String inTXT1;
+  private String inATNR;
+  private int XXCONO;
+  private String noseries01; 
+  private String txid;
+  private String faci;
+  private String carrier;
+  private String loader;
+  private String kfld;
+  private String oITNO;
+  private String oBANO;
+  private String oATNR;
+  private String lino; 
+  private String tx60;
+  private String oAVSQ;
+  private String oANSQ;
+  private String oTXID;
+  private List lstMatchedRecords;
+
+  public EXT851(LoggerAPI logger, DatabaseAPI database, BatchAPI batch, MICallerAPI miCaller, ProgramAPI program) {
+    this.logger = logger;
+    this.database = database;
+    this.batch = batch;
+  	this.miCaller = miCaller;
+  	this.program = program;
+  }
+  
+  public void main() {
+    
+    XXCONO= program.LDAZD.CONO;
+    divi = "";
+    inPYNO = "";
+    inATID = "";
+    inBREF = "";
+    inTXT1 = "";
+    inITNO = "";
+    inATVA = "";
+    inATNR = "";
+    
+    if (!batch.getReferenceId().isPresent()) {
+      logger.debug("Job data for job ${batch.getJobId()} is missing");
+      return;
+    }
+    
+    // Get parameters from EXTJOB
+    Optional<String> data = getJobData(batch.getReferenceId().get())
+    
+    if (!data.isPresent()) {
+      return
+    }
+    
+    String rawData = data.get();
+    String[] str;
+    str = rawData.split(',');
+    
+    if (str.length > 1) {
+      divi = str[0];
+      faci = str[1];
+      inITNO = str[2];
+      inBREF = str[3];
+      inATID = str[4];
+      inATVA = str[5];
+      inTXT1 = str[6];
+    } 
+    
+    noseries01 = "";
+    
+    lstMatchedRecords = new ArrayList();
+    ExpressionFactory expression = database.getExpressionFactory("MILOMA");
+    expression = expression.eq("LMBREF", inBREF);
+    expression = expression.and(expression.eq("LMFACI", faci));
+    DBAction query = database.table("MILOMA").index("00").matching(expression).selection("LMCONO", "LMBANO", "LMATNR").build();      
+    DBContainer container = query.getContainer();
+    container.set("LMCONO", XXCONO);
+    query.readAll(container, 1, 1000, releasedItemProcessor);
+  }
+  
+  /*
+	 * getJobData 
+	 *
+	*/
+  private Optional<String> getJobData(String referenceId) {
+    def queryEXTJOB = database.table("EXTJOB").index("00").selection("EXRFID", "EXJOID", "EXDATA").build();
+    def EXTJOB = queryEXTJOB.createContainer();
+    EXTJOB.set("EXCONO", XXCONO);
+    EXTJOB.set("EXRFID", referenceId);
+    if (queryEXTJOB.read(EXTJOB)) {
+      return Optional.of(EXTJOB.getString("EXDATA"));
+    }
+    return Optional.empty();
+  } 
+  
+  /*
+   * reading MILOMA and process attibutes  
+   *
+  */
+  Closure<?> releasedItemProcessor = { DBContainer MILOMA ->
+    oITNO = MILOMA.get("LMITNO").toString().trim();
+    oBANO = MILOMA.get("LMBANO").toString().trim();
+    oATNR = MILOMA.get("LMATNR").toString().trim();
+    
+    DBAction query = database.table("MIATTR").index("00").selection("AGAVSQ", "AGANSQ", "AGTXID").build();      
+    DBContainer container = query.getContainer();
+    container.set("AGCONO", XXCONO);
+    container.set("AGATNR", oATNR.toLong());
+    container.set("AGATID", 'CHM04');
+  
+    query.readAll(container, 3, 1000, releasedItemProcessor2);
+	  kfld = oATNR.padLeft(17,'') + 'CHM04'; 
+  
+    if (oTXID.toInteger() == 0) { 
+      Map<String, String> params = ["FILE": "MSYTXH"]; // toString is needed to convert from gstring to string
+      Closure<?> callback = {
+      Map<String, String> response ->
+      if(response.TXID != null){
+        txid = response.TXID.trim();  
+      }
+    }
+    miCaller.call("CRS980MI","RtvNewTextID", params, callback);	
+  
+    //addtexthead	
+	  Map<String, String> params01 = ["TXID": txid, "FILE": 'MIATTR00', "KFLD": kfld, "USID": program.getUser(), "TFIL": 'MSYTXH']; 
+    Closure<?> callback01 = {
+      Map<String, String> response ->
+    }
+    miCaller.call("CRS980MI","AddTxtBlockHead", params01, callback01);
+	
+	  //addtextbockline
+	  for (int i = 0; i < inTXT1.readLines().size(); i++) {
+      lino = (i + 1).toString();
+      tx60 = inTXT1.readLines().get(i);
+  
+	    Map<String, String> params02 = ["TXID": txid, "LINO": lino, "TX60": tx60, "TFIL": 'MSYTXH', "FILE": 'MIATTR00']; 
+      Closure<?> callback02 = {
+        Map<String, String> response ->
+      }
+      miCaller.call("CRS980MI","AddTxtBlockLine", params02, callback02);
+	  }
+  
+    //settextid
+    Map<String, String> params08 = ["FILE": "MIATTR00", "TXID": txid, "KV01": XXCONO.toString(), "KV02": oATNR.toString(), "KV03": "CHM04", "KV04": oAVSQ.toString(), "KV05": oANSQ.toString()]; 
+	   Closure<?> callback08 = {
+      Map<String, String> response ->
+    }
+    miCaller.call("CRS980MI","SetTextID", params08, callback08);
+    } else {
+      Map<String, String> params50 = ["TXID": oTXID, "TFIL": 'MSYTXH']; 
+      Closure<?> callback50 = {
+        Map<String, String> response ->
+      }
+      miCaller.call("CRS980MI","DltTxtBlockLins", params50, callback50);
+  
+	    Map<String, String> params51 = ["TXID": oTXID, "FILE": 'MIATTR00', "KFLD": kfld, "USID": program.getUser(), "TFIL": 'MSYTXH']; 
+      Closure<?> callback51 = {
+        Map<String, String> response ->
+      }
+      miCaller.call("CRS980MI","AddTxtBlockHead", params51, callback51);
+	
+	    for (int i = 0; i < inTXT1.readLines().size(); i++) {
+        lino = (i + 1).toString();
+        tx60 = inTXT1.readLines().get(i);
+  
+	      Map<String, String> params52 = ["TXID": oTXID, "LINO": lino, "TX60": tx60, "TFIL": 'MSYTXH', "FILE": 'MIATTR00']; 
+        Closure<?> callback52 = {
+          Map<String, String> response ->
+        }
+        miCaller.call("CRS980MI","AddTxtBlockLine", params52, callback52);
+	    }
+    }
+  }
+  
+  /*
+	 * get attribute values from MIATTR 
+	 *
+	*/
+  
+  Closure<?> releasedItemProcessor2 = { DBContainer MIATTR ->
+    oAVSQ = MIATTR.get("AGAVSQ").toString().trim();
+    oANSQ = MIATTR.get("AGANSQ").toString().trim();
+    oTXID = MIATTR.get("AGTXID").toString().trim();
+  }
+}
