a1296d253f53f63dc28b48a5c8d4c7adecc0cb8f
drirem0 <34474688+drirem0@users.noreply.github.com>
2024-11-21 20:24:41 +1100

    Update EXT850-Batch.groovy
    Fixes after code review

--- EXT850-Batch.groovy
+++ EXT850-Batch.groovy
@@ -20,21 +20,15 @@
  *                                                             *
  ***************************************************************
  */
- 
- import groovy.lang.Closure;
- import java.time.LocalDate;
- import java.time.LocalDateTime;
- import java.time.format.DateTimeFormatter;
- import java.text.DecimalFormat;
- import java.time.ZoneId;
- 
+
+
 /*
  *Modification area - M3
  *Nbr               Date      User id     Description
  *BF_R_2001         20241015  DRIESR      Add/Update Attributes in ATS101 for multiple lot numbers
  *
  */
- 
+
 public class EXT850 extends ExtendM3Batch {
   private final LoggerAPI logger;
   private final DatabaseAPI database;
@@ -42,7 +36,7 @@ public class EXT850 extends ExtendM3Batch {
   private final MICallerAPI miCaller;
   private final ProgramAPI program;
   private final IonAPI ion;
-  
+
   private String divi;
   private String faci;
   private String inPYNO;
@@ -53,7 +47,7 @@ public class EXT850 extends ExtendM3Batch {
   private String inATNR;
   private String errorM;
   private int XXCONO;
-  private String noseries01; 
+  private String noseries01;
   private int processed;
   private int maxrecs;
   private String carrier;
@@ -72,33 +66,31 @@ public class EXT850 extends ExtendM3Batch {
   	this.program = program;
   	this.ion = ion;
   }
-  
+
   public void main() {
-    
+
     XXCONO= program.LDAZD.CONO;
     divi = "";
-    inPYNO = "";
-    inATID = "";
-    inBREF = "";
+    faci = "";
     inITNO = "";
+    inBREF = "";
+    inATID = "";
     inATVA = "";
-    inATNR = "";
-    errorM = '';
-    
+
     if (!batch.getReferenceId().isPresent()) {
       return;
     }
-    
+
     Optional<String> data = getJobData(batch.getReferenceId().get());
-    
+
     if (!data.isPresent()) {
       return
     }
-    
+
     String rawData = data.get();
     String[] str;
     str = rawData.split(',');
-    
+
     if (str.length > 1) {
       divi = str[0];
       faci = str[1];
@@ -106,102 +98,102 @@ public class EXT850 extends ExtendM3Batch {
       inBREF = str[3];
       inATID = str[4];
       inATVA = str[5];
-    } 
-    
+    }
+
     noseries01 = "";
     processed = 0;
-    
+
     ExpressionFactory expression = database.getExpressionFactory("MILOMA");
     expression = expression.eq("LMCONO", XXCONO.toString());
     expression = expression.and(expression.eq("LMBREF", inBREF));
-    DBAction query = database.table("MILOMA").index("00").matching(expression).selection("LMCONO", "LMITNO", "LMBANO", "LMATNR").build();      
+    DBAction query = database.table("MILOMA").index("00").matching(expression).selection("LMCONO", "LMITNO", "LMBANO", "LMATNR").build();
     DBContainer container = query.getContainer();
     container.set("LMCONO", XXCONO);
     container.set("LMITNO", inITNO);
     query.readAll(container, 2, 1000, releasedItemProcessor);
   }
-  
+
   /*
-	 * getJobData 
+	 * getJobData
 	 *
 	*/
   private Optional<String> getJobData(String referenceId) {
-    def queryEXTJOB = database.table("EXTJOB").index("00").selection("EXRFID", "EXDATA").build();
-    def EXTJOB = queryEXTJOB.createContainer();
+    DBAction queryEXTJOB = database.table("EXTJOB").index("00").selection("EXRFID", "EXDATA").build();
+    DBContainer EXTJOB = queryEXTJOB.createContainer();
     EXTJOB.set("EXCONO", XXCONO);
     EXTJOB.set("EXRFID", referenceId);
     if (queryEXTJOB.read(EXTJOB)) {
       return Optional.of(EXTJOB.getString("EXDATA"));
     }
     return Optional.empty();
-  } 
-  
+  }
+
   /*
-   * reading MILOMA and process attibutes  
+   * reading MILOMA and process attibutes
    *
   */
   Closure<?> releasedItemProcessor = { DBContainer MILOMA ->
     oITNO = MILOMA.get("LMITNO").toString().trim();
     oBANO = MILOMA.get("LMBANO").toString().trim();
     oATNR = MILOMA.get("LMATNR").toString().trim();
- 
-    Map<String,String> params = ["ATNR": oATNR, "ATID": inATID, "ATVA": inATVA]; 
-    
-    Closure<?> callback = { 
+
+    Map<String,String> params = ["ATNR": oATNR, "ATID": inATID, "ATVA": inATVA];
+
+    Closure<?> callback = {
       Map<String, String> response ->
-      errorM = response.errorMessage; 
+      errorM = response.errorMessage;
     }
-    miCaller.call("ATS101MI","SetAttrValue", params, callback);	
-	  
+    miCaller.call("ATS101MI","SetAttrValue", params, callback);
+
 	  if(processed == 0 && errorM != null) { processerror(errorM);
 	  }
 
-	  Map<String,String> params01 = ["ATNR": oATNR]; 
-    
+	  Map<String,String> params01 = ["ATNR": oATNR];
+
     Closure<?> callback01 = {
       Map<String, String> response ->
       if(response.ATID.trim().equals("REC04")){
-        carrier = response.ATVA;  
+        carrier = response.ATVA;
       }
       if(response.ATID.trim().equals("REC05")){
-        loader = response.ATVA;  
+        loader = response.ATVA;
       }
       if(response.ATID.trim().equals("SUP01")){
-        supplier = response.ATVA;  
+        supplier = response.ATVA;
       }
       if(response.ATID.trim().equals("AGT01")){
-        agreement = response.ATVA;  
+        agreement = response.ATVA;
       }
     }
-    miCaller.call("ATS101MI","GetAttributes", params01, callback01);	 
+    miCaller.call("ATS101MI","GetAttributes", params01, callback01);
 
     if(carrier != null && supplier != null && agreement != null) {setLot(loader, carrier, supplier, agreement);
-    } 
+    }
   }
-  
+
   /*
-   * Process/distribute grading error report   
+   * Process/distribute grading error report
    *
   */
-  
+
   private void processerror(String errorm) {
-    Map<String,String> params60 = ["REPO": 'GRADE01', "REPV": 'GRADE01', "SUBJ": 'Error in S/Docket ' + inBREF, "EMTX": errorM, "SF1F": oBANO, "SF1T": oBANO ]; 
+    Map<String,String> params60 = ["REPO": 'GRADE01', "REPV": 'GRADE01', "SUBJ": 'Error in S/Docket ' + inBREF, "EMTX": errorM, "SF1F": oBANO, "SF1T": oBANO ];
     Closure<?> callback60 = {
       Map<String, String> response ->
     }
-    miCaller.call("AHS150MI","Submit", params60, callback60);	
+    miCaller.call("AHS150MI","Submit", params60, callback60);
     processed = 1;
   }
 
   /*
-   * Set loader/carrier/agreementno on lotnumber MILOMA   
+   * Set loader/carrier/agreementno on lotnumber MILOMA
    *
    */
   private void setLot(String loader, String carrier, String supplier, String agreement) {
-    Map<String,String> params02 = ["ITNO": oITNO, "BANO": oBANO, "ARLA": loader.trim(), "CFI2": carrier.trim(), "CFI3": agreement.trim(), "SUNO": supplier.trim() ]; 
+    Map<String,String> params02 = ["ITNO": oITNO, "BANO": oBANO, "ARLA": loader.trim(), "CFI2": carrier.trim(), "CFI3": agreement.trim(), "SUNO": supplier.trim() ];
     Closure<?> callback02 = {
       Map<String, String> response ->
     }
-    miCaller.call("MMS235MI","UpdItmLot", params02, callback02);	
+    miCaller.call("MMS235MI","UpdItmLot", params02, callback02);
   }
 }
